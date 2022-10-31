e5a9455b27c661ff2a1f5c82be71498296236594
Vaibhav Kushwaha <vaibhavvast6529@gmail.com>
2022-10-31 15:11:18 +0530

    searcBar and linear loading

--- src/components/coins/ListCoins.jsx
+++ src/components/coins/ListCoins.jsx
@@ -1,11 +1,22 @@
-import axios from "axios";
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  Container,
+  createTheme,
+  LinearProgress,
+  TableContainer,
+  TextField,
+  ThemeProvider,
+  Typography,
+} from "@material-ui/core";
 import { CoinList } from "../../config/api";
 import { CryptoState } from "../../CryptoContext";
+import Colors from "../../constants/colors";
 
 const ListCoins = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
 
   const { currency } = CryptoState();
 
@@ -17,13 +28,43 @@ const ListCoins = () => {
     setLoading(false);
   };
 
-  console.log("coins: ", coins,loading);
+  console.log("coins: ", coins, loading);
 
   useEffect(() => {
     fetchCoins();
   }, [currency]);
 
-  return <div>coins</div>;
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Container style={{ textAlign: "center" }}>
+        <Typography variant="h4" style={{ margin: 18, fontFamily: "Lato" }}>
+          Crptocurrency Prices by Market Cap
+        </Typography>
+        <TextField
+          label="Search For a Crypto Currency..."
+          variant="outlined"
+          style={{ marginBottom: 20, width: "100%" }}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <TableContainer>
+          {loading ? (
+            <LinearProgress style={{ backgroundColor: Colors.yellow }} />
+          ) : (
+            <></>
+          )}
+        </TableContainer>
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default ListCoins;
